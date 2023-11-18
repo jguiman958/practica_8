1818fb6c8b495d335e928f9539004f1d4af05519
Ubuntu <ubuntu@ip-172-31-90-182.ec2.internal>
2023-11-18 12:58:45 +0000

    prestachop_introduccion

---
+++ script/deploy_prestashop.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Muestra todos los comandos que se han ejeutado.
+
+set -ex
+
+# Actualización de repositorios
+ sudo apt update
+
+# Actualización de paquetes
+# sudo apt upgrade  
+
+# Incluimos las variables del archivo .env.
+source .env
+
+
+
+
+
+
