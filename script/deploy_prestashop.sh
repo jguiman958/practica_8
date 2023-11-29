5b686f9f81f8677dc282bddc7e469bbd7062ff03
Ubuntu <ubuntu@ip-172-31-91-118.ec2.internal>
2023-11-29 18:40:50 +0000

    ...

--- script/deploy_prestashop.sh
+++ script/deploy_prestashop.sh
@@ -11,25 +11,19 @@ apt update
 source .env
 
 # Borramos el contenido de /tmp
-rm -rf /tmp/prestashop_8.1.2.zip
+rm -rf /tmp/8.1.2.zip
 
 # Descargamos el codigo fuente de prestashop
-wget https://github.com/PrestaShop/PrestaShop/releases/download/8.1.2/prestashop_8.1.2.zip -P /tmp
-
-# Borramos el contenid de html
-rm -rf /var/www/html/*
-
-# Descomprimimos el .zip en /var/www/html
-unzip -u /tmp/prestashop_8.1.2.zip -d /var/www/html
+wget https://github.com/PrestaShop/PrestaShop/archive/refs/tags/8.1.2.zip -P /tmp
 
 # Eliminamos instalaciones previas de prestashop
 rm -rf /var/www/html/*
 
-# Movemos el contenido de prestashop a /var/www/html
-mv -f /tmp/PrestaShop-8.1.2  /var/www/html/
+# Descomprimimos el .zip en /var/www/html
+unzip -u /tmp/8.1.2.zip -d /var/www/html
 
 # Instalamos prestashop.
-php index_cli.php 
+php /var/www/html/PrestaShop-8.1.2/install-dev/index_cli.php
     --domain=$CERTIFICATE_DOMAIN \
     --db_server=$PRESTASHOP_DB_HOST \
     --db_name=$PRESTASHOP_DB_NAME \
@@ -37,7 +31,7 @@ php index_cli.php
     --db_password=$PRESTASHOP_DB_PASSWORD \
     --prefix=$PRESTASHOP_PREFIX \
     --email=$PRESTASHOP_EMAIL \
-    --password=$PRESTASHOP_PASSWORD 
+    --password=$PRESTASHOP_PASSWORD \
     --ssl=1
 
 # Damos permisos a www-data.
