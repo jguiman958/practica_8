54b0ceb82976664cad8d5b07b1dd6accaaab5e7f
jguiman958 <145347496+jguiman958@users.noreply.github.com>
2023-12-01 09:42:48 +0100

    Update deploy_prestashop.sh

--- script/deploy_prestashop.sh
+++ script/deploy_prestashop.sh
@@ -4,95 +4,71 @@
 set -x
 
 # Actualizamos los repos
-
 apt update -y
 
-#apt upgrade -y
-
 # Importación de las variables
-
 source .env
 
-#Instalación de extensiones de PHP:
-#--------------------------------------------------
-#1. BCMath Arbitrary Precision Mathematics (Recommended)
+# Instalación de extensiones de PHP:
+apt install php-curl -y
 
 apt install php-bcmath -y 
 
-#2. Image Processing and GD (Required and Recommended)
-
 apt install php-gd -y 
-
-#3. Internationalization Functions (Intl) (Required and Recommended)
 
 apt install php-intl -y 
 
-#4. Zip (Required and Recommended)
-
 apt install php-zip -y
-
-#5. Memcached y algunas tools (Recommended)
 
 apt install memcached -y
 
 apt install libmemcached-tools -y
 
-#6. Curl
-
-apt install php-curl -y
-
-#7. Cadena multibyte (Mbstring)
-
 apt install php-mbstring -y
 
-#8. 
+apt install php-dom php-xml
 
-apt-get install php-dom php-xml
-
-
+# Reiniciamos el servicio de apache2
 systemctl restart apache2
 
-#---------------------------------------------------
 
-#Modificación de configuración PHP:
-#--------------------------------------------------------------------------------
+#Configuración en php
 sed -i "s/memory_limit = 128M/$MEMORY_LIMIT/" /etc/php/8.1/apache2/php.ini
 sed -i "s/upload_max_filesize = 2M/$UPLOAD_MAX_FILESIZE/" /etc/php/8.1/apache2/php.ini
 sed -i "s/max_input_vars = 1000/$MAX_INPUT_VARS/" /etc/php/8.1/apache2/php.ini
 sed -i "s/post_max_size = 8M/$POST_MAX_SIZE/" /etc/php/8.1/apache2/php.ini
-#-------------------------------------------------------------------------------
+
 
 #Reinciar el siguiente archivo para que se aplique la configuración:
-
 systemctl restart apache2
 
-# Instalación de Prestashop
-
-#Borrar 
-
+#Borramos el instalador de prestashop.
 rm -rf /tmp/prestashop_8.1.2.zip
 
-# Traigo el codigo fuente de Prestashop
-
+# Descargo el codigo fuente de prestashop en /tmp
 wget https://github.com/PrestaShop/PrestaShop/releases/download/8.1.2/prestashop_8.1.2.zip -P /tmp
 
+# Borramos el contenido de html
 rm -rf /var/www/html/*
 
-# Lo descomprimo en /var/www/html
-
+# Descomprimos  el instalador de prestashop en /var/www/html
 unzip /tmp/prestashop_8.1.2.zip -d /var/www/html
 
+# Cambiamos el propietario de forma recursiva para el contenido de html
 chown  www-data:www-data /var/www/html/* -R
 
-#Creacion de usuario para la base de datos de Prestashop
-#----------------------------------------------------------------------------------------------------
+# Creacion de usuario para la base de datos de Prestashop y creación de la base de datos.
 mysql -u root <<< "DROP DATABASE IF EXISTS $PRESTASHOP_DB_NAME"
 mysql -u root <<< "CREATE DATABASE $PRESTASHOP_DB_NAME"
 mysql -u root <<< "DROP USER IF EXISTS '$PRESTASHOP_DB_USER'@'%'"
 mysql -u root <<< "CREATE USER '$PRESTASHOP_DB_USER'@'%'IDENTIFIED BY '$PRESTASHOP_DB_PASSWORD'"
 mysql -u root <<< "GRANT ALL PRIVILEGES ON $PRESTASHOP_DB_NAME.* TO '$PRESTASHOP_DB_USER'@'%'"
-#----------------------------------------------------------------------------------------------------
 
+# Reiniciamos el servicio de mysql.
 systemctl restart mysql.service
 
-unzip -u /var/www/html/prestashop.zip /var/www/html/prestashop
+# Copio el contenido de phpinfo.php con el dashboard de configuraciones de prestashop.
+cp ../conf/phpinfo.php /var/www/html
+
+# Reinicio el servicio de apache2
+systemctl restart apache2
