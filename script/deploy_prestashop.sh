f521aca1900f8802bb22926700f2e87f71bfa25b
Ubuntu <ubuntu@ip-172-31-91-118.ec2.internal>
2023-12-01 11:52:56 +0000

    ---

--- script/deploy_prestashop.sh
+++ script/deploy_prestashop.sh
@@ -9,12 +9,12 @@ apt update
 #Incluimos las variables env
 source .env
 
-#Creamos la base de datos y el usuario
-mysql -u root <<< "DROP DATABASE IF EXISTS $PS_DB_NAME"
-mysql -u root <<< "CREATE DATABASE $PS_DB_NAME"
-mysql -u root <<< "DROP USER IF EXISTS $PS_DB_USER@$PS_DB_SERVER"
-mysql -u root <<< "CREATE USER $PS_DB_USER@$PS_DB_SERVER IDENTIFIED BY '$PS_DB_PASSWORD'"
-mysql -u root <<< "GRANT ALL PRIVILEGES ON $PS_DB_NAME.* TO $PS_DB_USER@$PS_DB_SERVER"
+#Creamos la base de datos y el usuario para prestashop
+mysql -u root <<< "DROP DATABASE IF EXISTS $PRESTASHOP_DB_NAME"
+mysql -u root <<< "CREATE DATABASE $PRESTASHOP_DB_NAME"
+mysql -u root <<< "DROP USER IF EXISTS $PRESTASHOP_DB_USER@$IP_CLIENTE_MYSQL"
+mysql -u root <<< "CREATE USER $PRESTASHOP_DB_USER@$IP_CLIENTE_MYSQL IDENTIFIED BY '$PRESTASHOP_DB_PASSWORD'"
+mysql -u root <<< "GRANT ALL PRIVILEGES ON $PRESTASHOP_DB_NAME.* TO $PRESTASHOP_DB_USER@$IP_CLIENTE_MYSQL"
 
 #Borramos descargas previas
 rm -rf /tmp/prestashop_8.1.2.zip
@@ -29,14 +29,22 @@ apt install unzip -y
 rm -rf /var/www/html/*
 
 #Copiamos el phppsinfo para comprobaciones
-cp ../php/phppsinfo.php /var/www/html
+cp ../php/phppinfo.php /var/www/html
 
 #Descomprimimos el archivo y movemos su contenido
 unzip -u /tmp/prestashop_8.1.2.zip -d /tmp
+
+#Y muevo el contenido que contiene el instalador de prestashop, llamado prestashop.zip a /var/www/html
 unzip -u /tmp/prestashop.zip -d /var/www/html
 
 #Instalamos los paquetes necesarios de php
-apt install php-bcmath php-gd php-intl php-zip php-curl php-mbstring php-dom php-xml -y
+apt install php-bcmath -y 
+apt install php-gd -y
+apt install php-intl -y
+apt install php-zip -y
+apt install php-curl -y
+apt install php-mbstring -y
+apt install php-dom php-xml -y
 
 #Sustituimos las variables a las recomendadas por php
 sed -i "s/;max_input_vars = 1000/max_input_vars = 5000/" /etc/php/8.1/apache2/php.ini
@@ -52,18 +60,16 @@ chown -R www-data:www-data /var/www/html/*
 
 #Instalamos Prestashop
 php /var/www/html/install/index_cli.php \
-    --name=$PS_NAME \
-    --country=$PS_COUNTRY \
-    --firstname=$PS_FIRSTNAME \
-    --lastname=$PS_LASTNAME \
-    --password=$PS_PASSWORD \
-    --prefix=$PS_PREFIX \
-    --db_server=$PS_DB_SERVER \
-    --db_name=$PS_DB_NAME \
-    --db_user=$PS_DB_USER \
-    --db_password=$PS_DB_PASSWORD \
-    --domain=$CB_DOMAIN \
-    --email=$CB_EMAIL \
+    --name=$PRESTASHOP_NAME \
+    --country=$PRESTASHOP_COUNTRY \
+    --password=$PRESTASHOP_PASSWORD \
+    --prefix=$PRESTASHOP_PREFIX \
+    --db_server=$PRESTASHOP_DB_HOST \
+    --db_name=$PRESTASHOP_DB_NAME \
+    --db_user=$PRESTASHOP_DB_USER \
+    --db_password=$PRESTASHOP_DB_PASSWORD \
+    --domain=$CERTIFICATE_DOMAIN \
+    --email=$CERTIFICATE_EMAIL \
     --language=es \
     --ssl=1
 
